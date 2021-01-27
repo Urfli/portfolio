da089493b3a3a3630105b8034c2c5a9145ec3ae0
Urfli <urbannorbi95@gmail.com>
2021-01-27 14:39:31 +0100

    video section fixed

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -7,6 +7,7 @@ function VideoSection() {
         return (
             <div className='hero-container'>
               <video src='/videos/video-2.mov' autoPlay loop muted />
+              <div className='title'>Video loops</div>
             </div>
           );
   }
