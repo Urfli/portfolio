9b93cbe1bb985f2897c20f350a59da5671285afe
Zleed <tamasfloch95@gmail.com>
2021-01-31 19:05:17 +0100

    Updates

---
+++ precache-manifest.ee0d0cef4758b6653a4f356c483222fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76033d505daeb4595117affbcd9a640",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2b4423d49ad727967a9b",
+    "url": "/portfolio/static/css/main.75d6ac52.chunk.css"
+  },
+  {
+    "revision": "28b3ed6e6c3e22067de3",
+    "url": "/portfolio/static/js/2.b396d37f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.b396d37f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4423d49ad727967a9b",
+    "url": "/portfolio/static/js/main.0285b878.chunk.js"
+  },
+  {
+    "revision": "bcd29fd5c15690c5fb44",
+    "url": "/portfolio/static/js/runtime-main.a4855db3.js"
+  },
+  {
+    "revision": "b3f72f41b860e8524929108600e29dcb",
+    "url": "/portfolio/static/media/csokesz.b3f72f41.png"
+  },
+  {
+    "revision": "ed79c8487491c65a0a0000fbd8a0081b",
+    "url": "/portfolio/static/media/norfli.ed79c848.png"
+  }
+]);
